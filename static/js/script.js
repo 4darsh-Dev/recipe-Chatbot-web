05542e45abfa2af38625d6c05fa66a2133dc400d
= <adarshmaurya9415@gmail.com>
2023-07-11 23:39:32 +0530

    other pages

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,27 @@
 console.log("I am working properly!");
+
+const author = "Adarsh Maurya!";
+
+console.log(`Developer ${author}`);
+
+
+let menuBtnCond = true;
+let checkLabel = document.getElementById("check-label");
+
+const menuBtnChanger = function(){
+
+    if (menuBtnCond){
+        checkLabel.innerHTML = `<i class='bx bx-x'></i>`;
+        menuBtnCond = false;
+    }
+
+    else{
+        checkLabel.innerHTML = `<i class='bx bx-menu'></i>`;
+
+        menuBtnCond = true;
+    }
+
+}
+
+let menuBtn = document.getElementById("check");
+menuBtn.addEventListener("click", menuBtnChanger);

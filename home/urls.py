5131161264182667d645006b390b64069fea6fe8
= <adarshmaurya9415@gmail.com>
2023-07-11 23:49:27 +0530

    11 jun upd

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,9 @@ from home import views
 
 urlpatterns = [
     path("", views.index , name="home"),
+    # path("", views.index , name="home"),
+    path("about", views.about , name="about"),
+    path("contact", views.contact , name="contact"),
+    path("services", views.services , name="services"),
     
 ]
--- home/views.py
+++ home/views.py
@@ -5,7 +5,23 @@ from django.shortcuts import render,redirect
 # Create your views here.
 
 def index(request):
-    return render(request, "index.html") # you can also give variables , 
+    return render(request, "index.html") # you can also give variables ,
+
+
+ 
+def about(request):
+    return render(request, "about.html") 
+
+
+
+def services(request):
+    return render(request, "services.html") 
+
+
+def contact(request):
+    return render(request, "contact.html") 
+
+
 
 
 

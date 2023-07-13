58afe24e913f97284940c8ed9416565a5b58f66d
= <adarshmaurya9415@gmail.com>
2023-07-13 23:58:22 +0530

    recipe page addition

--- home/views.py
+++ home/views.py
@@ -11,7 +11,9 @@ def index(request):
  
 
 def recipe(request):
-    return render(request, "recipe.html") 
+    greeting = "Are you Hungry?"
+    return render(request, "recipe.html", {"greeting": greeting}) 
+    
 
 
  

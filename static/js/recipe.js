e3fe77e477b8d48ed81fc55bf78027ca7c4c9ba3
= <adarshmaurya9415@gmail.com>
2023-07-24 16:48:15 +0530

    changed the chatbot version to search results only

--- static/js/recipe.js
+++ static/js/recipe.js
@@ -1,29 +1,31 @@
-$(document).ready(function() {
-    // let botMessage = $('#bot-message');
-    var mainChatbox = $('#main-chbot');
-    var chatForm = $('#chat-form');
+// HANDLES THE CHATBOT RESPONSE SYSTEM 
 
-    function displayMessage(message) {
-        var messageElement = $('<p>').text(message);
-        mainChatbox.append(messageElement);
-    }
+// $(document).ready(function() {
+//     // let botMessage = $('#bot-message');
+//     var mainChatbox = $('#main-chbot');
+//     var chatForm = $('#chat-form');
 
-    function sendMessage(message) {
-        $.get('/recipe/process_message/', { message: message }, function(response) {
-            var reply = response.message;
-            displayMessage(reply);
-        });
-    }
+//     function displayMessage(message) {
+//         var messageElement = $('<p>').text(message);
+//         mainChatbox.append(messageElement);
+//     }
 
-    // Handle form submission
-    chatForm.submit(function(event) {
-        event.preventDefault();
-        var userInput =   $('#user-input').val() ;
-        displayMessage(userInput);
-        sendMessage($('#user-input').val());
-        $('#user-input').val('');
-    });
-});
+//     function sendMessage(message) {
+//         $.get('/recipe/process_message/', { message: message }, function(response) {
+//             var reply = response.message;
+//             displayMessage(reply);
+//         });
+//     }
+
+//     // Handle form submission
+//     chatForm.submit(function(event) {
+//         event.preventDefault();
+//         var userInput =   $('#user-input').val() ;
+//         displayMessage(userInput);
+//         sendMessage($('#user-input').val());
+//         $('#user-input').val('');
+//     });
+// });
 
 
 // Written
@@ -31,9 +33,20 @@ $(document).ready(function() {
 let messBtn = document.getElementById("mess-btn");
 let myFrom = document.getElementById("chat-form");
 
+
 const submitFm = ()=>{
     myFrom.submit();
+    console.log("searched");
 }
 
-messBtn.addEventListener('click',submitFm);
+messBtn.addEventListener("click", submitFm);
+
+myFrom.addEventListener('keyup', function(event){
+    if(event.code === 'Enter'){
+        event.preventDefault();
+        document.querySelector('form').submit();
+
+    }
+})
+
 

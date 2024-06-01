acc5567ce445c8188c10c80c6c7585eef0bd3a57
ADARSH MAURYA <109789509+4darsh-Dev@users.noreply.github.com>
2024-06-01 15:42:19 +0530

    added verce conf

--- recpieChatbot/urls.py
+++ recpieChatbot/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,3 +23,7 @@ urlpatterns = [
     path("recipe/", include('home.urls')),
     
 ]
+
+# vercel deployment conf
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
